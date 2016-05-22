e147d85f974aef890c60fba47cb38dcfcc7e474c
enricofer <enricofer@gmail.com>
2016-05-22 12:33:28 +0200

    packaging 0.1

---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import find_packages, setup
+
+import sys
+if sys.version_info[0] < 3:
+    print ("This package does not support Python 2.")
+    sys.exit(1)
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-warp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.9',
+        'psycopg2',
+        'django-cors-headers',
+        'django-http-proxy',
+        'python-slugify',
+        'django-imagekit',
+        'requests',
+    ],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A Django app for easy/collaborative georeferencing of raster datasets. Make use of GDAL library',
+    url='https://github.com/enricofer/django-warp',
+    author='Your Name',
+    author_email='enricofer@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',  # replace "X.Y" as appropriate
+        'Intended Audience :: Gis administrators',
+        'License :: OSI Approved :: GPL License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
