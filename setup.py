d6122420dd70f67f164f2c7594de9e372f1d8bb3
enrico <enricofer@gmail.com>
2017-08-02 14:08:04 +0200

    release 0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-warp',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     install_requires=[
         'Django>=1.9',
